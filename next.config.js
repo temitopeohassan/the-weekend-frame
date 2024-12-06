d15a3aa73175ac6f598a3fb23b29804b329af191
temitopeohassan <temitope.hassan.2@gmail.com>
2024-12-06 15:34:27 +0100

    next build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // your config options here
-  reactStrictMode: true,
-  // ... other options
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+        pathname: '/coins/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        pathname: '**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'warpcast.com',
+        pathname: '**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gravatar.com',
+        pathname: '**/*',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "punycode": false,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
