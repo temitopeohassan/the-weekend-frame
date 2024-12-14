e8989d8ee3dccb7d31e144a10348f31cb9a50d3b
temitopeohassan <temitope.hassan.2@gmail.com>
2024-12-14 07:18:52 +0100

    layout metadata check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,22 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
-const appUrl = process.env.NEXT_PUBLIC_URL || "https://the-weekend-frame-seven.vercel.app";
-
-const frame = {
-  version: "vNext",
-  image: `${appUrl}/opengraph-image`,
-  buttons: [
-    {
-      label: "Start Quiz",
-      action: "post"
-    }
-  ],
-  post_url: `${appUrl}/api/quiz`
-};
+import "~/app/globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL(appUrl),
-  title: "Stablecoin Personality Quiz",
-  description: "Which Stablecoin Are You?",
-  openGraph: {
-    title: "Stablecoin Personality Quiz",
-    description: "Which Stablecoin Are You?",
-    images: [{
-      url: `/opengraph-image`,
-      width: 600,
-      height: 400,
-    }],
-  },
-  other: {
-    "fc:frame": JSON.stringify(frame),
-  },
+  title: "The Weekend Frame",
+  description: "What Stablecoin Are You?",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-white dark:bg-gray-900">
+        {children}
+      </body>
     </html>
   );
 }
